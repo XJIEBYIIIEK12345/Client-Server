f2b7cae1becf2bb744bc14296b7d1d7e4fb01a5a
XJIEBYIIIEK12345 <vada461@gmail.com>
2026-07-21 12:07:24 +0300

    Добавлен ввод порта через консоль для сервера

--- Server/main.cpp
+++ Server/main.cpp
@@ -1,12 +1,20 @@
 #include <QCoreApplication>
 #include "Server.h"
 #include <QRandomGenerator>
+#include <QCommandLineParser>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    quint16 port = 6789;
+    QCommandLineParser parser;
+    parser.addHelpOption();
+    parser.addVersionOption();
+    QCommandLineOption portOption (QStringList() << "port", "Port", "port", "6789");
+    parser.addOption(portOption);
+    parser.process(a);
+
+    quint16 port = parser.value(portOption).toUShort();
 
     Server server(port);
 
